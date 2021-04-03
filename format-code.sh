1ced3473d0322a9d3ab6bc1576cb21eafe3725ce
David Goosen <david@davidgoosen.kiwi>
2021-04-04 11:23:10 +1200

    Setup autoformatting CMakeList.txt files

--- format-code.sh
+++ format-code.sh
@@ -2,3 +2,6 @@
 
 find src test -name '*.cpp' -exec clang-format -i {} \;
 find src test -name '*.h' -exec clang-format -i {} \;
+
+find . -path ./build -prune -false -o -name "CMakeLists.txt" \
+    -exec cmake-format -c .cmake-format.yaml -i {} \;
